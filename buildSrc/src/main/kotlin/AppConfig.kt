2e0617b218e5a489842e42e7004b2dd6f44f9700
Yuchan <dbc12176@gmail.com>
2022-07-28 01:12:49 +0900

    Kotlin DSL 적용
    Clean architecture 를 멀티 모듈로 만들어 관리 하기 때문에 라이브러리 버전, AppConfig 들을 한번에 관리하기 용이한 Kotlin DSL 적용
    
    AppConfig.kt
    -> Application Config 파일을 관리
    
    Dependencies.kt
    -> Library 파일을 관리

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,17 @@
+
+object AppConfig{
+    const val ApplicationId = "com.devsurfer.devtodonote_cleanarchitecture"
+    const val CompileSdkVersion = 32
+    const val MinSdkVersion = 27
+    const val TargetSdkVersion = 32
+    const val VersionCode = 1
+    const val VersionName ="1.0.0"
+
+    const val TestInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    const val DefaultProguardFile = "proguard-android-optimize.txt"
+    const val ProguardRules = "proguard-rules.pro"
+    const val ConsumerProguardRules = "consumer-rules.pro"
+
+    const val JvmTarget = "1.8"
+}
