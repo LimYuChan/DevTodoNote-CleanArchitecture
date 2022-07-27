5d13e2b0445c0ea06ff699a742b2998b8ac81d1b
Yuchan <dbc12176@gmail.com>
2022-07-28 02:52:13 +0900

    settings.gradle type 변경 (groovy -> kotlin dsl)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "DevTodoNote-CleanArchitecture"
-include ':presentation'
-include ':data'
-include ':domain'
+include(":presentation", ":data", ":domain")
